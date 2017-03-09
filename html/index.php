bb4e9a027983e4b52dac7b83fc2d912361dac3db
savonfourbe <reconyooy@gmail.com>
2017-03-09 01:59:25 +0100

    web app

---
+++ html/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>YH - web creation</title>
+  <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="../materialize/css/materialize.css">
+
+  <link rel="stylesheet" href="../css/style-principal.css">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  
+</head>
+<body>
+  <?php
+    include 'navigation.php';
+  ?>
+  
+
+  <script src="../jquery/jquery-3.1.1.min.js"></script>
+  <script src="../materialize/js/materialize.js"></script>
+  
+  <script src="../js/initialisation-menu.js"></script>
+</body>
+</html>
---
+++ html/navigation.php
@@ -0,0 +1,15 @@
+<ul id="slide-out" class="side-nav">
+    <li><div class="userView ">
+      <div class="background" style="background-color:#333333">
+      
+      </div>
+      <a href="#!user"><center><img class="circle" src="../img/younes.jpg"></center></a>
+      <a href="#!name"><span class="white-text name center-align">Younes Hamdoune</span></a>
+      <a href="#!email"><span class="white-text email center-align">younes.hamdoune.cv@gmail.com</span></a>
+    </div></li>
+    <li><a href="index.php">accueil</a></li>
+    <li><div class="divider"></div></li>
+    <li><a class="subheader">projets</a></li>
+    <li><a class="waves-effect" href="#!"><i class="material-icons">music_note</i>NOISY</a></li>
+  </ul>
+  <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons" style="font-size:8vh;z-index:100000;">menu</i></a>
